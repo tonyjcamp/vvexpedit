be955109e8ceb7ea5368753cada48b130c27662e
tonyjcamp <tonyjcamp@gmail.com>
2017-10-26 21:32:45 -0700

    async/await discog-service.js

--- services/discog-service.js
+++ services/discog-service.js
@@ -9,50 +9,38 @@ const dis = new Discogs({
 
 const db = dis.database()
 
-exports.search = (q, page) => {
+exports.search = async (q, page) => {
   const options = {type: ['artist'], page, per_page: 10}
-
-  return db.search(q, options).then( (results) => {
-    return results
-  })
+  const results = await db.search(q, options)
+  return results
 }
 
-exports.getArtist = (id) => {
-
-  return db.getArtist(id).then((data) => {
-    const {id} = data
-
-    return db.getArtistReleases(id).then( (releaseData) => {
-      const {releases} = releaseData
-      return {data, releases}
-    })
-
-  })
+exports.getArtist = async (id) => {
+  const data = await db.getArtist(id)
+  const releaseData = await db.getArtistReleases(data.id)
+  const {releases} = releaseData
+  return {data, releases}
 }
 
-exports.getReleaseDetails = (id) => {
-  return db.getRelease(id).then((details) => {
-    return {details}
-  })
+exports.getReleaseDetails = async (id) => {
+  const details = await db.getRelease(id)
+  return {details}
 }
 
-exports.getMasterDetails = (id) => {
-  return db.getMaster(id).then((data) => {
-    const {images} = data
-    return {data, images}
-  })
+exports.getMasterDetails = async (id) => {
+  const data = await db.getMaster(id)
+  const {images} = data
+  return {data, images}
 }
 
 // change the expected parameter to be req.session.discogsAccount
 // instead of the whole req
-exports.getCollectionFolders = (req) => {
+exports.getCollectionFolders = async (req) => {
   const discogsUser = new Discogs(req.session.discogsAccount)
   const userCollection = discogsUser.user().collection()
-  return discogsUser.getIdentity().then( (data) => {
-    return userCollection.getFolders(data.username).then( (collectionFolders) => {
-        return {data, collectionFolders}
-    })
-  })
+  const data = await discogsUser.getIdentity()
+  const collectionFolders = await userCollection.getFolders(data.username)
+  return {data, collectionFolders}
 }
 
 exports.getFolder = async (discogsAccount, folderId) => {
