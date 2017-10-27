f90f6d1e524ef72fca8a008e52cac93135fa6b34
Chad Watson <cwatson@remarkablehealth.com>
2017-10-27 13:55:26 -0700

    Move Discog Service to new ES6 Class style
    * Found out this helps with documentation and also some perf for V8
    * No functionality was changed just syntax
    * Edit controller to use new DiscogService
    * Removed old service file

--- controllers/discogsController.js
+++ controllers/discogsController.js
@@ -1,46 +1,58 @@
-const discogService = require('../services/discog-service');
+const Discog = require('../services/discog');
+const DiscogService = new Discog({
+	consumerKey: process.env.DISCOGS_KEY,
+	consumerSecret: process.env.DISCOGS_SECRET
+});
 
 exports.home = (req, res) => {
 	res.render('home');
 };
 
-exports.search = (req, res) => {
-	const { q } = req.query;
-	const page = req.query.page ? req.query.page : 1;
-
-	discogService.search(q, page).then((results) => {
+exports.search = async (req, res, next) => {
+	try {
+		const { q } = req.query;
+		const page = req.query.page ? req.query.page : 1;
+		var results = await DiscogService.search(q, page);
 		res.render('results', { results, q });
-	});
+	} catch (error) {
+		next(error);
+	}
 };
 
-exports.getReleaseDetails = (req, res) => {
-	const { id } = req.params;
-
-	discogService.getReleaseDetails(id).then((details) => {
+exports.getReleaseDetails = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		var releaseDetails = await DiscogService.getReleaseDetails(id);
 		res.render('release', details);
-	});
+	} catch (error) {
+		next(error);
+	}
 };
 
-exports.getMasterDetails = (req, res) => {
-	const { id } = req.params;
-
-	discogService.getMasterDetails(id).then((data) => {
-		res.render('master', data);
-	});
+exports.getMasterDetails = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		var masterDetails = await DiscogService.getMasterDetails(id);
+		res.render('master', masterDetails);
+	} catch (error) {
+		next(error);
+	}
 };
 
-exports.getArtistDetails = (req, res) => {
-	const { id } = req.params;
-
-	discogService.getArtist(id).then((artist) => {
+exports.getArtistDetails = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		var artist = await DiscogService.getArtist(id);
 		res.render('artist', artist);
-	});
+	} catch (error) {
+		next(error);
+	}
 };
 
 // Discogs USER specific functions
 exports.getCollectionFolders = async (req, res, next) => {
 	try {
-		const accountData = await discogService.getCollectionFolders(req);
+		const accountData = await DiscogService.getCollectionFolders(req);
 		const { data, collectionFolders } = accountData;
 		res.render('folders', { data, folders: collectionFolders.folders });
 	} catch (error) {
@@ -48,9 +60,12 @@ exports.getCollectionFolders = async (req, res, next) => {
 	}
 };
 
-exports.getFolder = (req, res) => {
-	discogService.getFolder(req.session.discogsAccount, req.params.id).then((folder) => {
+exports.getFolder = async (req, res, next) => {
+	try {
+		var folder = await DiscogService.getFolder(req.session.discogsAccount, req.params.id);
 		const { releases } = folder;
 		res.render('folder', { releases });
-	});
+	} catch (error) {
+		next(error);
+	}
 };
